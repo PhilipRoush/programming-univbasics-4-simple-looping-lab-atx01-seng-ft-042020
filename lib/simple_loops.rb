de860e331d85d17ab35dd8f09b00ab635e00cdb5
Philip Roush <roush.philip@gmail.com>
2020-04-05 20:16:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,10 @@
 # Write your methods here
 def loop_message_five_times(message)
-  message == ""
+  message == "Hello world."
   count = 0
   while count < 5 do
+    puts message 
+    count += 1 
+  end
+end
     
